d13026cd16a1409840b8170d88cdff1c899ca814
alitoccacieli <82356938+alitoccacieli@users.noreply.github.com>
2022-09-12 18:35:59 +0200

    Update server.py

--- server.py
+++ server.py
@@ -172,6 +172,5 @@ def thisRoute():
     
 
 if __name__ == "__main__" :
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True,host='127.0.0.1', port=5000)
 
- #(debug=True, host='0.0.0.0', port=5000) # run the flask app#app.run(debug=True)
